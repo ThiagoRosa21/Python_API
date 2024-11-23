a4e1f67d5f419e64c715c850125459bbc19832e5
Thiago Rosa <thiagorosadasilva39@gmail.com>
2024-11-23 13:48:58 -0300

    V1 Program

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+books =[
+    {
+        'id':1,
+        'título':'Harry Potter',
+        'autor': 'Thiago Rosa'
+    },
+    
+    {
+        'id':2,
+        'título':'Among us',
+        'autor':'Lucas Augusto'
+        
+    },
+    
+    {
+        'id':3,
+        'título':'Lógica de Programação',
+        'autor':'joão dário'
+        
+    }
+]
+
+
+#consulta de todos os livros
+@app.route('/books',methods=['GET'])
+def get_books():
+    return jsonify(books)
+
+
+# Consulta por ID
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book_id(id):
+    for book in books: 
+        if book.get('id') == id:
+            return jsonify(book)
+    return jsonify({'error': 'Book not found'}), 404
+
+# edição de livro id
+@app.route('/books/<int:id>', methods=['PUT'])
+def book_edit(id):
+    changed_book = request.get_json()  
+    for index, book in enumerate(books):
+        if book.get('id') == id:
+            books[index].update(changed_book)  
+            return jsonify(books[index]) 
+    return jsonify({'error': 'Book not found'}), 404 
+
+# Adicionar Livros
+@app.route('/books',methods=['POST'])
+def add_book():
+    new_book = request.get_json()
+    books.append(new_book) 
+    return jsonify(books)
+
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    for index, book in enumerate(books):
+        if book.get('id') == id:
+            del books[index] 
+            return jsonify({'message': 'Book deleted successfully!', 'books': books})
+    return jsonify({'error': 'Book not found'}), 404
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='localhost', debug=True)
